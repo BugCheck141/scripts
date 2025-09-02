2405ae931cd874c46e66937f7baca4e1940c82ff
Chris Ho <30272533+BugCheck141@users.noreply.github.com>
2025-09-02 11:19:09 -0400

    Update printEDID.ps1
    add CIM version

--- printEDID.ps1
+++ printEDID.ps1
@@ -10,3 +10,18 @@
 			}
 		}
   }
+
+# CIM version (for PS7)
+Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorDescriptorMethods |
+ForEach-Object {
+    Write-Output $_.InstanceName
+    for ($i = 0; $i -le 10; $i++) {
+        try {
+            $res = Invoke-CimMethod -InputObject $_ -MethodName 'WmiGetMonitorRawEEdidV1Block' -Arguments @{ BlockId = [uint32]$i } -ErrorAction Stop
+            if ($null -eq $res -or -not $res.PSObject.Properties.Match('BlockContent')) { break }
+            Write-Output ( ($res.BlockContent | ForEach-Object { '{0:X2}' -f $_ }) -join ' ' )
+        } catch {
+            break
+        }
+    }
+}
